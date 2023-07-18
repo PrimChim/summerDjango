1b95d1d754d44759e7bd48f78f4a48ac6b5fdb50
PrimChim <85182130+PrimChim@users.noreply.github.com>
2023-07-18 10:13:22 +0545

    static css and dynamic html from django

--- migrations/views.py
+++ migrations/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Blog #manager objects
 
 def index(request):
-    return render(request, "migrations/index.html")
+    blog = Blog.objects.all()
+    
+
+    return render(request, "migrations/index.html", {"blogs":blog})
 
 def about(request):
     return render(request, 'migrations/about.html')
