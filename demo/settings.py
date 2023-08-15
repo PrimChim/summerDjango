625e83fbbe386da37bd47c1c6a11d6b1545c41ef
PrimChim <85182130+PrimChim@users.noreply.github.com>
2023-08-15 09:27:46 +0545

    allowed host changed

--- demo/settings.py
+++ demo/settings.py
@@ -16,9 +16,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+
+
+ALLOWED_HOSTS = ['https://blog-application-y1q2.onrender.com/']
 
 
 # Application definition
