d920de137ea036cfdcdc772b00d262022c2a846b
PrimChim <85182130+PrimChim@users.noreply.github.com>
2023-08-15 09:12:51 +0545

    postgresql added

--- demo/settings.py
+++ demo/settings.py
@@ -2,6 +2,9 @@ import os
 
 from pathlib import Path
 
+import dj_database_url
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -10,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zlqxn1m07l3$$yc1v2$y+cofv!*va)#+6ypdg1l5d&2@4z@%se'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -66,10 +69,11 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        config('DATABASE'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    ),
 }
 
 
@@ -108,13 +112,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
+STATICFILES_ROOT = [
     os.path.join(BASE_DIR, 'static')
 ]
+
 EMAIL_BACKEND= "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST= "smtp.gmail.com"
-EMAIL_HOST_USER= "np03cs4s210050@heraldcollege.edu.np"
-EMAIL_HOST_PASSWORD= ""
+EMAIL_HOST_USER= config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD= config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT= 587
 EMAIL_USE_TLS= True
 DEFAULT_FROM_EMAIL= "np03cs4s210050@heraldcollege.edu.np"
