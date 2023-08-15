2bfce71abf5462d5c4a9922780e417f94289c4fe
PrimChim <85182130+PrimChim@users.noreply.github.com>
2023-08-15 10:14:55 +0545

    static solved

--- demo/settings.py
+++ demo/settings.py
@@ -20,7 +20,7 @@ DEBUG = True
 
 
 
-ALLOWED_HOSTS = ['blog-application-y1q2.onrender.com','blog.pritamgurung.com.np']
+ALLOWED_HOSTS = ['127.0.0.1','blog-application-y1q2.onrender.com','blog.pritamgurung.com.np']
 
 
 # Application definition
@@ -114,8 +114,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,"static")
+# ]
 
 
 EMAIL_BACKEND= "django.core.mail.backends.smtp.EmailBackend"
